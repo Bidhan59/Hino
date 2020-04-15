7f9ed4c354ab26181c767dc4e67b3b2c8565c01c
Bidhan <sarkarbidhan59@gmail.com>
2020-04-15 18:25:39 +0530

    Changes handled in models

--- models/devices.js
+++ models/devices.js
@@ -4,6 +4,7 @@ const root = require("app-root-path");
 const db = require(`${root}/config/db`);
 const dbDetails = require(`${root}/config/secret.js`);
 const jsonSQL = require("json-sql")();
+var SQLGenerator = require("sql-json-generator");
 module.exports.getDevices = function() {
   var q = Q.defer();
   sql
@@ -13,10 +14,8 @@ module.exports.getDevices = function() {
       var sqlQuery = "SELECT * FROM Devices";
       var req = new sql.Request();
       req.query(sqlQuery).then((result, err) => {
-        console.log("came here");
         if (err) console.log(err);
         else {
-          console.log("recordset>>>>>>>>>>>>>", result);
           q.resolve(result.recordset);
         }
         sql.close();
@@ -30,21 +29,16 @@ module.exports.getDevices = function() {
 
 module.exports.postDevices = function(data) {
   var q = Q.defer();
-  var sqlQ = jsonSQL.build({
-    type: "insert",
-    table: "Devices",
-    values: data
-  });
   sql
     .connect(dbDetails)
     .then(function(config, err) {
       if (err) console.log("err>>>>>>>>>>>", err);
       console.log("database connected");
       var req = new sql.Request();
-      req.query(sqlQ).then((result, err) => {
+      var query = queryFormat(data);
+      req.query(query).then((result, err) => {
         if (err) console.log(err);
         else {
-          console.log("recordset>>>>>>>>>>>>>", result);
           q.resolve("Data Inserted");
         }
         sql.close();
@@ -55,3 +49,20 @@ module.exports.postDevices = function(data) {
     });
   return q.promise;
 };
+
+function queryFormat(data) {
+  var values =
+    "(" +
+    data.id +
+    "," +
+    "'" +
+    data.name +
+    "'" +
+    "," +
+    "'" +
+    data.Device_Details +
+    "'" +
+    ")";
+  var query = "INSERT INTO Devices (id, name, Device_Details) VALUES " + values;
+  return query;
+}
